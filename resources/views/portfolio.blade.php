262a162fbe36724aaf05a6e121d752155341b3fc
Sadoudi Anis <sadoudimohamedanis@gmail.com>
2020-07-06 16:47:07 +0100

    changes recommended
    Signed-off-by: Sadoudi Anis <sadoudimohamedanis@gmail.com>

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Portfolio')
+@section('title', __('Portfolio'))
 
 @section('content')
 
--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Services')
+@section('title', __('Services'))
 
 @section('content')
 
